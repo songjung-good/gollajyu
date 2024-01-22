af9549cd819bf7a5b59551a11503e6fe170335d7
HocheolHwang <inpofes7849@naver.com>
2024-01-22 11:41:52 +0900

    New:: Page 파일 추가

---
+++ front-gollajyu/src/pages/BroadcastPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1>지금골라쥬</h1>
+    </div>
+  );
+};
+
+export default MainPage;
---
+++ front-gollajyu/src/pages/VotePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1>투표모아쥬</h1>
+    </div>
+  );
+};
+
+export default MainPage;
