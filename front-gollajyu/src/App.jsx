0be1d122834745930071f904e6e2fc9e678987f1
HocheolHwang <inpofes7849@naver.com>
2024-01-22 11:42:36 +0900

    Update:: App.jsx 라우터링크 수정

--- front-gollajyu/src/App.jsx
+++ front-gollajyu/src/App.jsx
@@ -1,15 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
+import MainPage from "./pages/MainPage";
+import VotePage from "./pages/VotePage";
+import BroadcastPage from "./pages/BroadcastPage";
+import StatisticPage from "./pages/StatisticPage";
+import TestPage from "./pages/TestPage";
+import MyPage from "./pages/MyPage";
 
-function App() {
+const App = () => {
   return (
     <Router>
-      <div>
-        <NavigationBar />
-      </div>
+      <NavigationBar />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/VotePage" element={<VotePage />} />
+        <Route path="/BroadcastPage" element={<BroadcastPage />} />
+        <Route path="/StatisticPage" element={<StatisticPage />} />
+        <Route path="/TestPage" element={<TestPage />} />
+        <Route path="/MyPage" element={<MyPage />} />
+      </Routes>
     </Router>
   );
-}
+};
 
 export default App;
