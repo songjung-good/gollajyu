b29ebfee94f11f6fc62925fd70b22a392bf329e6
HocheolHwang <inpofes7849@naver.com>
2024-01-22 11:23:07 +0900

    Feat:: 네비게이션 바 코드 작성

---
+++ front-gollajyu/src/components/NavigationBar.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const NavigationBar = () => {
+  const isLoggedIn = true; // 로그인 상태
+//   const isLoggedIn = false; // 로그아웃 상태
+
+  return (
+    <nav style={navigationBarStyle}>
+      <ul style={listStyle}>
+        <li style={listItemStyle}>
+          <NavLink to="/">골라쥬</NavLink>
+        </li>
+        <li style={listItemStyle}>
+          <NavLink to="/VotePage">투표모아쥬</NavLink>
+        </li>
+        <li style={listItemStyle}>
+          <NavLink to="/BroadcastPage">지금골라쥬</NavLink>
+        </li>
+        <li style={listItemStyle}>
+          <NavLink to="/StatisticPage">통계보여쥬</NavLink>
+        </li>
+        <li style={listItemStyle}>
+          <NavLink to="/TestPage">소비성향알려쥬</NavLink>
+        </li>
+        <li style={{ ...listItemStyle, marginLeft: "auto" }}> {/* 오른쪽 정렬 */}
+          {isLoggedIn ? (
+            <>
+              <NavLink to="/MyPage">내프로필</NavLink>
+              <span style={{ margin: "0 5px" }}>|</span>
+              <button>로그아웃</button>
+            </>
+          ) : (
+            <>
+              <NavLink to="/Login">로그인</NavLink>
+              <span style={{ margin: "0 5px" }}>|</span>
+              <NavLink to="/SignUp">회원가입</NavLink>
+            </>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+const navigationBarStyle = {
+  background: "#4A4A4A",
+  padding: "10px",
+};
+
+const listStyle = {
+  listStyleType: "none",
+  display: "flex",  // 목록을 수평으로 배치
+};
+
+const listItemStyle = {
+  marginRight: "10px",
+};
+
+export default NavigationBar;
