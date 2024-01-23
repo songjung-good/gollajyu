b9ca7ae82e5412c27b9b5dcbd5bafdb2566382df
HocheolHwang <inpofes7849@naver.com>
2024-01-23 09:41:39 +0900

    Update:: 투표 버튼 App.jsx에 추가

--- front-gollajyu/src/App.jsx
+++ front-gollajyu/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavigationBar from "./components/NavigationBar";
+import VoteButton from "./components/VoteButton";
 import MainPage from "./pages/MainPage";
 import VotePage from "./pages/VotePage";
 import BroadcastPage from "./pages/BroadcastPage";
@@ -13,6 +14,7 @@ const App = () => {
   return (
     <Router>
       <NavigationBar />
+      <VoteButton />
       <div className="body">
         <Routes>
           <Route path="/" element={<MainPage />} />
