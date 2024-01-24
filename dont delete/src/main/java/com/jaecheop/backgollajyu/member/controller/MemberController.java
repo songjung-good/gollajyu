d67efd10fb56832278c3dac12b836aa7169061d6
김하영 <ha09368@naver.com>
2024-01-24 12:13:26 +0900

    Feature:: 로그인 기능 추가

--- dont delete/src/main/java/com/jaecheop/backgollajyu/member/controller/MemberController.java
+++ dont delete/src/main/java/com/jaecheop/backgollajyu/member/controller/MemberController.java
@@ -1,9 +1,31 @@
 package com.jaecheop.backgollajyu.member.controller;
 
+import com.jaecheop.backgollajyu.member.model.LoginReqDto;
+import com.jaecheop.backgollajyu.member.model.LoginResDto;
+import com.jaecheop.backgollajyu.member.service.MemberService;
+import com.jaecheop.backgollajyu.vote.model.ResponseMessage;
+import com.jaecheop.backgollajyu.vote.model.ServiceResult;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/members")
+@RequiredArgsConstructor
 public class MemberController {
 
+    private final MemberService memberService;
 
+    @PostMapping("/login")
+    public ResponseEntity<ResponseMessage> login(@RequestBody LoginReqDto loginReqDto, HttpSession session){
+        ServiceResult result = memberService.login(loginReqDto, session);
+        if(!result.isResult()){
+            return ResponseEntity.ok().body(ResponseMessage.fail(result.getMessage()));
+        }
+        return ResponseEntity.ok().body(ResponseMessage.success(result.getData()));
+    }
 }
--- dont delete/src/main/java/com/jaecheop/backgollajyu/member/model/Birthday.java
+++ dont delete/src/main/java/com/jaecheop/backgollajyu/member/model/Birthday.java
@@ -1,8 +1,16 @@
 package com.jaecheop.backgollajyu.member.model;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class Birthday {
     private int year;
     private int month;
