9ca69413a3db3152b38d4aa698d539b64bcdb44a
HocheolHwang <inpofes7849@naver.com>
2024-01-24 17:53:00 +0900

    Update:: 사이드 바 경로 변경, Route 주소 변경

--- front-gollajyu/src/pages/MyPage.jsx
+++ front-gollajyu/src/pages/MyPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import SideBarContainer from "../components/SideBarContainer";
+import SideBar from "../components/SideBar";
 import MyProfile from "../components/MyProfile";
 import MyActivities from "../components/MyActivities";
 
@@ -9,16 +9,16 @@ const MyPage = () => {
     marginTop: "100px",
     marginLeft: "300px",
     width: "100%",
-    height: "1200px",
+    height: "1000px",
   }
 
   return (
     <div>
-      <SideBarContainer />
+      <SideBar />
       <div style={body}>
         <Routes>
-          <Route path="/MyPage" element={<MyProfile />} />
-          <Route path="/MyPage/MyActivities" element={<MyActivities />} />
+          <Route path="/" element={<MyProfile />} />
+          <Route path="/MyActivities" element={<MyActivities />} />
         </Routes>
       </div>
     </div>
