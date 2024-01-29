bfa83376b8ece176731e23dfc073fa18490429b3
HocheolHwang <inpofes7849@naver.com>
2024-01-29 15:35:58 +0900

    Update:: 프로필 페이지 세부 수정

--- front-gollajyu/src/components/MyProfile.jsx
+++ front-gollajyu/src/components/MyProfile.jsx
@@ -1,7 +1,20 @@
 import React, { useState } from "react";
+import { useMediaQuery } from "react-responsive";
 import DefaultProfileImage from "/assets/images/default_profile_img.png";
+import { fontSize } from "@mui/system";
 
 const MyProfile = () => {
+  // ----------- 반응형 웹페이지 구현 -----------
+  const isLarge = useMediaQuery({
+    query: "(min-width:1024px)",
+  });
+  const isMedium = useMediaQuery({
+    query : "(min-width:768px) and (max-width:1024px)"
+  });
+  const isSmall = useMediaQuery({
+    query : "(max-width:768px)"
+  });
+  
   // ----------- 버튼 hover -----------
   const [
     informationButtonHovered,
@@ -16,38 +29,53 @@ const MyProfile = () => {
 
   // --------------------------------- css 시작 ---------------------------------
 
+  // ----------- 컨텐츠 컨테이너 스타일 -----------
   const containerStyle = {
+    // 디자인
     marginBottom: "50px",
   };
 
+  // ----------- 제목 및 버튼 컨테이너 스타일 -----------
   const titleButtonContainerStyle = {
+    // 디자인
+    marginBottom: "20px",
+    height: "60px",
+
+    // 컨텐츠 정렬
     display: "flex",
     alignItems: "center",
-    marginBottom: "20px",
   };
 
-  const meduimFontStyle = {
-    fontFamily: "GmarketSansMedium",
+  // ----------- 제목 스타일 -----------
+  const titleTextStyle = {
+  // 디자인
+  marginTop: "5px",
+
+  // 글자
+  fontSize: "32px",
   };
 
+  // ----------- 큰 글자 스타일 -----------
   const bigTextStyle = {
-    ...meduimFontStyle,
+    // 글자
     fontSize: "24px",
   };
 
+  // ----------- 중간 글자 스타일 -----------
   const mediumTextStyle = {
-    ...meduimFontStyle,
+    // 글자
     fontSize: "20px",
     color: "#4A4A4A", // 글자 색: 회색
   };
 
+  // ----------- 작은 글자 스타일 -----------
   const smallTextStyle = {
+    // 글자
     fontSize: "20px",
   };
 
+  // ----------- 버튼 스타일 -----------
   const buttonStyle = {
-    ...meduimFontStyle,
-
     // 디자인
     marginLeft: "20px",
     width: "90px", // 버튼 넓이: 90px
@@ -61,75 +89,121 @@ const MyProfile = () => {
     color: "#9C9C9C", // 글자 색: 회색
   };
 
+  // ----------- 기본정보 수정하기 버튼 스타일 -----------
   const informationButtonStyle = {
+    // 상속
     ...buttonStyle,
+
+    // 디자인
     background: informationButtonHovered ? "#D9D9D9" : "#FFFFFF", // 마우스 호버 시 배경 색상 변경
   };
 
+  // ----------- 소비성향조사 다시하기 버튼 스타일 -----------
   const testButtonStyle = {
+    // 상속
     ...buttonStyle,
+
+    // 디자인
     background: testButtonHovered ? "#D9D9D9" : "#FFFFFF", // 마우스 호버 시 배경 색상 변경
   };
 
+  // ----------- 프로필 컨테이너 스타일 -----------
   const profileContainerStyle = {
+    // 디자인
     padding: "40px",
     borderRadius: "50px",
     background: "#FFFFFF",
   };
 
+  // ----------- 내 정보 컨테이너 스타일 -----------
   const infoContainerStyle = {
+    // 컨텐츠 정렬
     display: "flex",
     alignItems: "center",
   };
 
+  // ----------- 프로필 이미지 스타일 -----------
   const profileImageStyle = {
+    // 디자인
+    marginRight: "40px",
     width: "100px",
     height: "100px",
     borderRadius: "50%",
-    marginRight: "40px",
   };
 
+  // ----------- 닉네임 스타일 -----------
   const nicknameStyle = {
+    // 상속
     ...bigTextStyle,
+
+    // 디자인
     marginTop: "20px",
   };
 
+  // ----------- 이메일 스타일 -----------
   const emailStyle = {
+    // 상속
     ...mediumTextStyle,
+
+    // 디자인
     marginTop: "10px",
   };
 
+  // ----------- 구분선 스타일 -----------
   const barStyle = {
+    // 디자인
     margin: "30px 0",
     height: "3px",
     width: "100%",
     backgroundColor: "#F0F0F0",
   };
 
+  // ----------- 내 정보 아이템 스타일 -----------
   const infoItemStyle = {
+    // 디자인
+    margin: "10px 0",
+    padding: "10px 20px",
+    width: isSmall? "100%" : "50%",
+    height: "60px",
+    backgroundColor: "#F0F0F0",
+
+    // 컨텐츠 정렬
     display: "flex",
     justifyContent: "space-between", // 항목 균일 간격으로 정렬
     alignItems: "center",
-    padding: "10px 20px",
-    width: "50%",
-    backgroundColor: "#F0F0F0",
   };
 
+  // ----------- 왼쪽 아이템 스타일 -----------
   const itemLeftStyle = {
+    // 상속
     ...infoItemStyle,
-    marginRight: "20px",
+
+    // 디자인
+    marginRight: isSmall ? "0px" : "20px",
   };
 
+  // ----------- 오른쪽 아이템 스타일 -----------
   const itemRightStyle = {
+    // 상속
     ...infoItemStyle,
-    marginLeft: "20px",
+
+    // 디자인
+    marginLeft: isSmall ? "0px" : "20px",
   };
 
+  // ----------- 아이템 텍스트 스타일 -----------
+  const itemTextStyle = {
+    // 글자
+    fontSize: !isLarge ? "22px" : "24px"
+  }
+
+  // ----------- 소비성향 이미지 스타일 -----------
   const testImageStyle = {
+    // 디자인
+    marginRight: "20px",
     width: "50px",
     height: "50px",
     borderRadius: "50%",
-    marginRight: "20px",
   };
 
   // --------------------------------- css 끝 ---------------------------------
@@ -137,9 +211,10 @@ const MyProfile = () => {
 
   return (
     <>
+      {/* ------------- 기본정보 ------------- */}
       <div style={containerStyle}>
         <div style={titleButtonContainerStyle}>
-          <span style={bigTextStyle}>기본정보</span>
+          <span style={titleTextStyle}>기본정보</span>
           <button
             style={informationButtonStyle}
             onMouseOver={() => setInformationButtonHovered(true)}
@@ -161,22 +236,26 @@ const MyProfile = () => {
             </div>
           </div>
           <div style={barStyle}></div>
-          <div style={infoContainerStyle}>
+          <div style={{
+            ...infoContainerStyle,
+            flexDirection: isSmall? "column" : "row"
+          }}>
             <div style={itemLeftStyle}>
-              <div style={bigTextStyle}>생년월일</div>
+              <div style={itemTextStyle}>생년월일</div>
               <div style={mediumTextStyle}>[생년월일]</div>
             </div>
             <div style={itemRightStyle}>
-              <div style={bigTextStyle}>성별</div>
+              <div style={itemTextStyle}>성별</div>
               <div style={mediumTextStyle}>[성별]</div>
             </div>
           </div>
         </div>
       </div>
 
+      {/* ------------- 소비성향 ------------- */}
       <div style={containerStyle}>
         <div style={titleButtonContainerStyle}>
-          <span style={bigTextStyle}>소비성향</span>
+          <span style={titleTextStyle}>소비성향</span>
           <button
             style={testButtonStyle}
             onMouseOver={() => setTestButtonHovered(true)}
