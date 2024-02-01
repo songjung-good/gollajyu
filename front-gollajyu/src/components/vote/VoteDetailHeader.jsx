9494b6677acc1cba16be1ee9887b0c813e4c327e
배영환 <jed595@hanmail.net>
2024-02-01 17:19:52 +0900

    Feat:: 투표결과의 드롭다운 추가

--- front-gollajyu/src/components/vote/VoteDetailHeader.jsx
+++ front-gollajyu/src/components/vote/VoteDetailHeader.jsx
@@ -8,14 +8,14 @@ const VoteDetailHeader = ( props ) => {
           <div className="w-auto h-auto bg-gray-200 rounded-lg border border-black mr-4 flex items-center justify-center">
             <span className="text-base font-normal">{props.category}</span>
           </div>
-          <div className="flex items-center">
+          <div className="flex items-center font-color-red">
             ♥<span className="text-base font-normal ml-2">{props.likes}</span>
           </div>
           <div className="flex items-center ml-4">
             ρ<span className="text-base font-normal ml-2">{props.participants}</span>
           </div>
         </div>
-        <button className="w-auto h-auto bg-gray-200 rounded-full flex items-center justify-center">X</button>
+        <button className="w-auto h-auto bg-red-200 rounded-full flex items-center justify-center">X</button>
       </div>
       <div className="p-4">
         <span className="text-xl font-normal">{props.author} | {props.createdAt}</span>
