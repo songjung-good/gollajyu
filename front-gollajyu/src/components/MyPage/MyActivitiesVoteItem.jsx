6d560feaf0c3d353847440b93343a5daa3536194
HocheolHwang <inpofes7849@naver.com>
2024-02-06 12:36:38 +0900

    Refactor:: 띄어쓰기 수정

--- front-gollajyu/src/components/MyPage/MyActivitiesVoteItem.jsx
+++ front-gollajyu/src/components/MyPage/MyActivitiesVoteItem.jsx
@@ -16,7 +16,7 @@ const MyActivitiesVoteItem = () => {
 
   // ----------- 반응형 웹페이지 구현 -----------
   const isXLarge = useMediaQuery({
-    query: "(min-width:1024px)",
+    query : "(min-width:1024px)",
   });
   const isLarge = useMediaQuery({
     query : "(min-width:768px) and (max-width:1023.98px)"
