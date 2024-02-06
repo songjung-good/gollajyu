ceed981f0f629eb991474283c3b396dcfbd92939
HocheolHwang <inpofes7849@naver.com>
2024-02-06 17:21:00 +0900

    Update:: 차트 반응형 디자인 개선 및 유형 미선택 시 전체 -> ' - '으로 표시 변경

--- front-gollajyu/src/components/StatisticPage/StatisticPageChart.jsx
+++ front-gollajyu/src/components/StatisticPage/StatisticPageChart.jsx
@@ -75,24 +75,68 @@ const StatisticPageChart = ({ selectedCategory, itemCount, selectedRadioValues, 
     // 디자인
     width: "100%",
     height:
-      isXLarge ? "600px" :
-      isLarge ? "500px" :
-      isMedium ? "400px" : "300px",
+      isXLarge ? "800px" :
+      isLarge ? "700px" :
+      isMedium ? "600px" : "500px",
   };
 
+  // ----------- flex 컨테이너 스타일 -----------
+  const flexContainerStyle = {
+    // 컨텐츠 정렬
+    display: "flex",
+    alignItems: "center",
+  };
+
+  // ----------- legned 컨테이너 스타일 -----------
+  const legendContainerStyle = {
+    // 상속
+    ...flexContainerStyle,
+
+    // 컨텐츠 정렬
+    flexWrap: "wrap", // 가로 길이를 넘어가면 줄바꿈
+  }
+
+  // ----------- legned 아이템 스타일 -----------
+  const legendItemStyle = {
+    // 상속
+    ...flexContainerStyle,
+
+    // 디자인
+    flex: isXLarge || isLarge ? "0 0 50%" : "0 0 100%", // 한 줄에 최대 2개/1개의 항목
+    marginBottom: '10px', // 각 항목 사이의 간격
+  }
+
+  // ----------- legned 아이콘 스타일 -----------
+  const legendIconStyle = {
+    // 디자인
+    marginRight: "5px",
+    marginBottom:
+      isXLarge ? "5px" :
+      isLarge ? "4px" : "3px",
+    width:
+      isXLarge ? "20px" :
+      isLarge ? "17px" :
+      isMedium ? "14px" : "11px",
+    height:
+      isXLarge ? "20px" :
+      isLarge ? "17px" :
+      isMedium ? "14px" : "11px",
+  }
+
   // --------------------------------- css 끝 ---------------------------------
+
 
   // ----------- itemCount 수 만큼 Radar를 생성 -----------
   const radars = [];
   for (let i = 1; i <= itemCount; i++) {
-    const ageValue = selectedRadioValues[`나이-${i}`] || '전체';
-    const genderValue = selectedRadioValues[`성별-${i}`] || '전체';
-    const tasteValue = selectedDropdownValues[`소비성향-${i}`] || '전체';
+    const ageValue = selectedRadioValues[`나이-${i}`] || ' - ';
+    const genderValue = selectedRadioValues[`성별-${i}`] || ' - ';
+    const tasteValue = selectedDropdownValues[`소비성향-${i}`] || ' - ';
 
     radars.push(
       <Radar
         key={`유형 ${i}`}
-        name={`${ageValue}/${genderValue}/${tasteValue}`}
+        name={`유형 ${i}: ${ageValue}/${genderValue}/${tasteValue}`}
         dataKey={String.fromCharCode(64 + i)}  // A, B, C, D
         stroke={colorList[i-1]}
         fill={colorList[i-1]}
@@ -101,16 +145,43 @@ const StatisticPageChart = ({ selectedCategory, itemCount, selectedRadioValues, 
     );
   }
 
+  // ----------- 커스텀 레전드 렌더링 함수 -----------
+  const CustomLegend = (props) => {
+    const { payload } = props;
+    return (
+      <ul style={legendContainerStyle}>
+        {payload.map((entry, index) => (
+          <li key={`item-${index}`} style={legendItemStyle}>
+            <div style={{
+              ...legendIconStyle,
+              backgroundColor: entry.color,
+            }}></div>
+            <span className="fontsize-md" style={{ color: entry.color }}>{entry.value}</span>
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
   return (
     <>
       <div style={containerStyle}>
         <ResponsiveContainer width="100%" height="100%">
-          <RadarChart cx="50%" cy="50%" outerRadius="80%" data={selectedTagData}>
+          <RadarChart
+            cx="50%"
+            cy={ isXLarge || isLarge ? "44%" : "37%" }
+            outerRadius={
+              isXLarge ? 300 :
+              isLarge ? 250 :
+              isMedium ? 190 : 130
+            }
+            data={selectedTagData}
+          >
             <PolarGrid />
             <PolarAngleAxis dataKey="subject" />
             <PolarRadiusAxis angle={54} domain={[0, 150]} />
             {radars}
-            <Legend />
+            <Legend content={<CustomLegend />} />
           </RadarChart>
         </ResponsiveContainer>
       </div>
