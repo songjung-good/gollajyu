1b08f35e4529f596d809c75d33f195efbc368fed
이상훈 <dltkdgns0303@gmail.com>
2024-02-07 17:52:09 +0900

    Feat:: 투표 모아쥬 페이지 투표 띄우기 완성

--- front-gollajyu/src/pages/VotePage.jsx
+++ front-gollajyu/src/pages/VotePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import VotePageList from "../components/vote/VotePageList";
 import VotePageHeader from "../components/vote/VotePageHeader";
 import TmpModal from "../components/TmpModal"; // 임시 모달
@@ -6,25 +6,54 @@ import VoteButton from "../components/VoteButton";
 import useModalStore from "../stores/modalState";
 import API_URL from "../stores/apiURL";
 import axios from "axios";
+import useAuthStore from "../stores/userState";
 
 const VotePage = () => {
-  // 정렬 상태
-  const [sortType, setSortType] = useState(null);
-  // 정렬 함수
+  const user = useAuthStore((state) => state.user);
+  const [voteListData, setVoteListData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [prevVoteList, setPrevVoteList] = useState();
+
+
+// 정렬 함수
   const handleSort = (type) => {
-    setSortType(type);
+    try {
+      let sortedData;
+      if (type === 'popular') {
+        sortedData = voteListData.slice().sort((a, b) => b.likesCnt - a.likesCnt);
+      } else {
+        sortedData = prevVoteList;
+      }
+      setVoteListData(sortedData);
+    } catch (error) {
+      console.error('정렬 또는 검색 중 오류 발생:', error);
+    }
   };
 
   // 검색 카테고리
-  const [searchCategory, setSearchCategory] = useState();
+  const [searchCategory, setSearchCategory] = useState(0);
 
   // 검색어 상태
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState(null);
   // 검색 함수
-  const handleSearch = () => {
-    fetchData(searchCategory, searchTerm);
-    console.log(searchCategory, searchTerm)
+  const handleSearch = async () => {
+    try {
+      const searchData = await fetchData(searchCategory, searchTerm);
+      await setVoteListData(searchData.body.voteList);
+      await setPrevVoteList(searchData.body.voteList);
+      setIsLoading(false);
+      console.log(searchCategory, searchTerm);
+    } catch (error) {
+      // 오류 처리
+      console.error('데이터 가져오기 실패:', error);
+      setIsLoading(false);
+    }
   };
+  useEffect(() => {
+    // Fetch data when the page is turned on
+    handleSearch();
+    console.log("렌더링");
+  }, []);
 
 
 // searchCategory와 searchTerm을 매개변수로 받는 함수
@@ -59,12 +88,22 @@ const fetchData = async (categoryId, keyword) => {
   const isVoteProductCreateModalOpened = useModalStore(
     (state) => state.isVoteProductCreateModalOpened
   );
+
+
+
   return (
     <div>
       <VoteButton />
       <VotePageHeader onSort={handleSort} onSearchTerm={setSearchTerm} onSearchCategory={setSearchCategory} onSearch={handleSearch} />{" "}
       {/* 정렬 함수를 props로 전달 */}
-      <VotePageList sortType={sortType} searchTerm={searchTerm} />{" "}
+      <div>
+      {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+        <div><VotePageList voteList={voteListData}/>{" "}
+        </div>
+        )
+      }</div>
       {/* 정렬 상태를 props로 전달 */}
       {isVoteDetailModalOpened && <TmpModal></TmpModal>}
       {isVoteSimpleCreateModalOpened && <TmpModal></TmpModal>}
