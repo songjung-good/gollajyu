e13e9b5863f1c4ef91d6a1b40c3bbc2c15705422
이시은 <lsy53430@naver.com>
2024-02-07 14:35:12 +0900

    Feat:: 내 활동 요약 서버와 연결
    TODO
    투표 상세 모달 연결
    작성, 좋아요, 참여 투표에 [댓글] 부분 수정

--- front-gollajyu/src/components/MyPage/MyActivitiesLiked.jsx
+++ front-gollajyu/src/components/MyPage/MyActivitiesLiked.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import MyActivitiesVoteItem from "./MyActivitiesVoteItem";
 
-const MyActivitiesLiked = (props) => {
-  console.log("좋아요한 투표:", props);
+const MyActivitiesLiked = ({ props }) => {
+  // console.log("좋아요한 투표:", props);
   return (
     <>
-      <MyActivitiesVoteItem />
-      <MyActivitiesVoteItem />
-      <MyActivitiesVoteItem />
-      <MyActivitiesVoteItem />
+      {props.length >= 1 ? (
+        props.map((item, index) => {
+          return <MyActivitiesVoteItem key={index} voteItem={item} />;
+        })
+      ) : (
+        <p>좋아요한 투표가 없습니다</p>
+      )}
     </>
   );
 };
--- front-gollajyu/src/components/MyPage/MyActivitiesParticipated.jsx
+++ front-gollajyu/src/components/MyPage/MyActivitiesParticipated.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import MyActivitiesVoteItem from "./MyActivitiesVoteItem";
 
-const MyActivitiesParticipated = (props) => {
-  console.log("참여한 투표:", props);
+const MyActivitiesParticipated = ({ props }) => {
+  // console.log("참여한 투표:", props);
   return (
     <>
-      <MyActivitiesVoteItem />
-      <MyActivitiesVoteItem />
-      <MyActivitiesVoteItem />
-      <MyActivitiesVoteItem />
+      {props.length >= 1 ? (
+        props.map((item, index) => {
+          return <MyActivitiesVoteItem key={index} voteItem={item} />;
+        })
+      ) : (
+        <p>참여한 투표가 없습니다</p>
+      )}
     </>
   );
 };
