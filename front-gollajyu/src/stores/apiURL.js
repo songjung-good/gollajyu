3efebffb245560390ed2102911c683afc71e5051
송승준 <80227755+seungjun123@users.noreply.github.com>
2024-02-08 10:20:07 +0900

    Update:: API_URL 수정

--- front-gollajyu/src/stores/apiURL.js
+++ front-gollajyu/src/stores/apiURL.js
@@ -1,6 +1,8 @@
-const API_URL =
-  process.env.NODE_ENV !== "production"
-    ? "http://localhost:8080/api"
-    : "https://i10e107.p.ssafy.io/api";
+// const API_URL =
+//   process.env.NODE_ENV !== "production"
+//     ? "http://localhost:8080/api"
+//     : "https://i10e107.p.ssafy.io/api";
+
+    const API_URL = "https://i10e107.p.ssafy.io/api";
 
 export default API_URL;
