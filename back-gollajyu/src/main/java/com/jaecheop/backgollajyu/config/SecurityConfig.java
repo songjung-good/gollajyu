90fb81249af39c03be50c52d774ece1280e3c7a8
송승준 <80227755+seungjun123@users.noreply.github.com>
2024-02-08 12:15:21 +0900

    Update:: 환경 변수 설정 분리(Develop)

--- back-gollajyu/src/main/java/com/jaecheop/backgollajyu/config/SecurityConfig.java
+++ back-gollajyu/src/main/java/com/jaecheop/backgollajyu/config/SecurityConfig.java
@@ -2,9 +2,9 @@ package com.jaecheop.backgollajyu.config;
 
 import com.jaecheop.backgollajyu.socialLogin.CustomOAuth2UserService;
 import com.jaecheop.backgollajyu.socialLogin.Oauth2LoginSuccessHandler;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,7 +13,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -27,12 +26,14 @@ import java.util.List;
 public class SecurityConfig {
     private final CustomOAuth2UserService customOAuth2UserService;
 
+    @Value("${api.url}")
+    private String apiUrl;
+
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(List.of("https://i10E107.p.ssafy.io"));
-//        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(apiUrl));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(Arrays.asList("set-cookie"));
