6c3889ac18fc1d06303f433d97e34c5721d4c7d8
HocheolHwang <inpofes7849@naver.com>
2024-02-12 20:52:05 +0900

    Feat:: 단어에 맞는 접미사 추가 및 스타일 변경

--- front-gollajyu/src/components/MainPage/MainWord.jsx
+++ front-gollajyu/src/components/MainPage/MainWord.jsx
@@ -1,86 +1,165 @@
 // 리액트 및 훅/라이브러리
 import React, { useState, useEffect } from 'react';
-import tagColorData from '/src/stores/tagColorData';
+
 // 반응형 웹 디자인을 위한 유틸리티 함수
-import { useResponsiveQueries } from "../../stores/responsiveUtils";
+import { useResponsiveQueries } from "/src/stores/responsiveUtils";
+
+// 태그 색상 데이터 불러오기
+import tagColorData from '/src/stores/tagColorData';
 
 
-// 임시 데이터
+// 카테고리, 태그 및 유저 정보 데이터
 const categories = ['의류', '가구', '신발', '전자제품']
-const tags1 = ['기능성', '브랜드', '소재', '색감', '모양']
-const tags2 = ['가성비', '브랜드', '디자인', '기능성', '내구성']
-const userInfo1 = ['남성', '여성']
-const userInfo2 =['10대','20대', '30대', '40대', '50대 이상']
-const userInfo3 = ['프렌치 마카롱', '티라미수', '포춘쿠키', 
-                    '지하철 만쥬', '곤약젤리', '오곡라떼', 
-                    '콜라', '고구마 말랭이', '붕어빵', 
-                    '나초', '에너지바', '슈크림', '식빵', 
-                    '민트초코', '초코파이', '초코잼']
+const tagTypes1 = ['기능성', '브랜드', '소재', '색감', '모양']
+const tagTypes2 = ['가성비', '브랜드', '디자인', '기능성', '내구성']
+const ageTypes = ['10대','20대', '30대', '40대', '50대 이상']
+const genderTypes = ['남성', '여성']
+const testTypes = [
+  '프렌치 마카롱', '티라미수', '포춘쿠키', 
+  '지하철 만쥬', '곤약젤리', '오곡라떼', 
+  '콜라', '고구마 말랭이', '붕어빵', 
+  '나초', '에너지바', '슈크림', '식빵', 
+  '민트초코', '초코파이', '초코잼'
+]
 
-// 랜덤 인덱스
+// 각 태그 및 소비성향 별 접미사 정보 데이터
+const tagsSuffix1 = ['을', '를', '를', '을', '을']
+const tagsSuffix2 = ['를', '를', '을', '을', '을']
+const userInfoSuffix = [
+  '은', '는', '는', '는', '는', '는', '는', '는',
+  '은', '는', '는', '은', '은', '는', '는', '은'
+]
+
+// 랜덤 인덱스 반환 함수
 const getRandomItem = (array) => {
   const index = Math.floor(Math.random() * array.length);
   return array[index];
 };
 
-// 문장 생성
+// 랜덤 카테고리 인덱스 생성
+const firstCategoryIndex = Math.floor(Math.random() * 4);
+let secondCategoryIndex;
+
+// 두 카테고리 인덱스가 서로 다르도록 2번째 카테고리를 설정
+do {
+  secondCategoryIndex = Math.floor(Math.random() * 4);
+} while (secondCategoryIndex === firstCategoryIndex);
+
+// 랜덤 태그 인덱스 생성
+const firstTagIndex = Math.floor(Math.random() * 5);
+const secondTagIndex = Math.floor(Math.random() * 5);
+
+
+// ------------------ 문장 생성 함수 ------------------
 const generateSentence = () => {
-  const categoriesIndex = Math.floor(Math.random() * categories.length);
-  const tags = categoriesIndex < 2 ? tags1 : tags2;
-  const userInfo = [userInfo1, userInfo2, userInfo3].map(getRandomItem);
-  const words = `${categories[categoriesIndex]} 에서 ${getRandomItem(tags)} 을 선호하는 
-  ${userInfo[0]} ${userInfo[1]} ${userInfo[2]}은/는 
-  ${getRandomItem(categories)} 에서 ${getRandomItem(tags)} 을 선호해요`;
+
+  // 카테고리 종류에 따라 태그 및 접미사 종류 변경
+  const firstTagTypes = firstCategoryIndex < 2 ? tagTypes1 : tagTypes2;
+  const secondTagTypes = secondCategoryIndex < 2 ? tagTypes1 : tagTypes2;
+  const firstTagSuffix = firstCategoryIndex < 2 ? tagsSuffix1 : tagsSuffix2;
+  const secondSuffix = secondCategoryIndex < 2 ? tagsSuffix1 : tagsSuffix2;
+
+  // 랜덤 유저 정보 생성
+  const userInfo = [ageTypes, genderTypes, testTypes].map(getRandomItem);
+
+  // 문장 생성 및 반환
+  const words = `${categories[firstCategoryIndex]} 에서 ${firstTagTypes[firstTagIndex]} ${firstTagSuffix[firstTagIndex]} 선호하는
+" ${userInfo[0]} - ${userInfo[1]} - ${userInfo[2]} " ${userInfoSuffix[testTypes.indexOf(userInfo[2])]}
+${categories[secondCategoryIndex]} 에서 ${secondTagTypes[secondTagIndex]} ${secondSuffix[secondTagIndex]} 선호해요`;
 
   return words;
 };
 
-// 색상 지정
-const colors = {
-  // 의류: '#FF595E',
-  // 가구: '#FFCA3A',
-  // 신발: '#8AC926',
-  // 전자제품: '#1982C4',
-  // // 기능성: '#5EC4DC',
-  // 브랜드: '#D395D0',
-  // 소재: '#CDE05A',
-  // 색감: '#FFA959',
-  // 모양: '#8CB0F6',
-  // 가성비: '#8FD9B6',
-  // 디자인: '#FF9999',
-  // 내구성: '#FFDF38',
-  // 남성: '#FF7F50',
-  // 여성: '#FFC107',
-  // '10대': '#9400D3',
-  // '20대': '#00BFFF',
-  // '30대': '#FF7F50',
-  // '40대': '#FFC107',
-  // '50대 이상': '#9400D3',
-};
 
-// 문장 출력 및 css
 const MainWord = () => {
+
+  // ------------------ 반응형 웹페이지 구현 ------------------
+  const { isXLarge, isLarge, isMedium, isSmall } = useResponsiveQueries();
+
+  // phrase 상태 변수 선언 및 초기값 빈 문자열로 설정
   const [phrase, setPhrase] = useState('');
 
+  // 컴포넌트가 마운트될 때마다 문구가 바뀌도록 설정
   useEffect(() => {
     setPhrase(generateSentence());
   }, []);
 
+  // --------------------------------- css 시작 ---------------------------------
+
+  // ----------- body 스타일 -----------
+  const bodyStyle = {
+    // 디자인
+    margin: "0 auto", // 가로 중앙 정렬
+    paddingTop: "50px", // 상단 여백: 50px
+    width:
+      isXLarge ? "1000px" :
+      isLarge ? "740px" :
+      isMedium ? "470px" : "375px",
+  };
+
+  // ----------- flex 컨테이너 스타일 -----------
+  const flexContainerStyle = {
+    // 컨텐츠 정렬
+    display: "flex",
+    alignItems: "center",
+  };
+  
+  // ----------- 문구 컨테이너 스타일 -----------
+  const wordContainerStyle = {
+    // 상속
+    ...flexContainerStyle,
+
+    // 컨텐츠 정렬
+    justifyContent: "center",
+  }
+
+  // ----------- 따옴표 스타일 -----------
+  const quotesStyle = {
+    // 디자인
+    margin: "0 40px",
+    display: isXLarge ? "block" : "none", 
+
+    // 글자
+    fontSize: "160px",
+    color: "#FFFFFF"
+  }
+
+  // ----------- 메인 문구 스타일 -----------
+  const mainWordStyle = {
+    // 글자
+    fontSize: 
+      isXLarge ? "42px" :
+      isLarge ? "38px" :
+      isMedium ? "34px" : "30px",
+    textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+  }
+
+  // --------------------------------- css 끝 ---------------------------------
+
+
   return (
-    <div className="px-20 pt-20 w-auto h-auto relative text-center flex justify-center items-center">
-      <div className="flex-grow md:flex-grow-4">
-        <div className="whitespace-pre-wrap text-pretty font-extrabold text-black fontsize-xl tracking-wider">
-          {phrase.split(' ').map((word, index) => {
-            const tagColor = tagColorData.find(tag => tag.name === word)?.color;
-            return (
-              <span key={index} style={{ color: tagColor || colors[word], fontSize: 'inherit' }}>
-                {word + ' '}
-              </span>
-            );
-          })}
+    <>
+      <div style={bodyStyle}>
+        <div style={wordContainerStyle}>
+          <div style={quotesStyle}>“</div>
+          <div className="text-center whitespace-pre-wrap font-extrabold tracking-wider">
+            {phrase.split(' ').map((word, index) => {
+              const tagColor = tagColorData.find(tag => tag.name === word)?.color;
+              return (
+                <span key={index} style={{
+                  ...mainWordStyle,
+                  color: tagColor || "#FFFFFF",
+                }}
+                >
+                  {word + ' '}
+                </span>
+              );
+            })}
+          </div>
+          <div style={quotesStyle}>”</div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
