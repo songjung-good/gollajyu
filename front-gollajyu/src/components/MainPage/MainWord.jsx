d1feef7438d1713966213ac175f4f8cba60f6742
HocheolHwang <inpofes7849@naver.com>
2024-02-13 15:40:51 +0900

    Update:: 문구 스타일 변경

--- front-gollajyu/src/components/MainPage/MainWord.jsx
+++ front-gollajyu/src/components/MainPage/MainWord.jsx
@@ -1,5 +1,6 @@
 // 리액트 및 훅/라이브러리
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 // 반응형 웹 디자인을 위한 유틸리티 함수
 import { useResponsiveQueries } from "/src/stores/responsiveUtils";
@@ -64,8 +65,8 @@ const generateSentence = () => {
 
   // 문장 생성 및 반환
   const words = `${categories[firstCategoryIndex]} 에서 ${firstTagTypes[firstTagIndex]} ${firstTagSuffix[firstTagIndex]} 선호하는
-" ${userInfo[0]} - ${userInfo[1]} - ${userInfo[2]} " ${userInfoSuffix[testTypes.indexOf(userInfo[2])]}
-${categories[secondCategoryIndex]} 에서 ${secondTagTypes[secondTagIndex]} ${secondSuffix[secondTagIndex]} 선호해요`;
+    “ ${userInfo[0]} ${userInfo[1]} ${userInfo[2]} ” ${userInfoSuffix[testTypes.indexOf(userInfo[2])]}
+    ${categories[secondCategoryIndex]} 에서 ${secondTagTypes[secondTagIndex]} ${secondSuffix[secondTagIndex]} 선호해요`;
 
   return words;
 };
@@ -86,52 +87,70 @@ const MainWord = () => {
 
   // --------------------------------- css 시작 ---------------------------------
 
-  // ----------- body 스타일 -----------
-  const bodyStyle = {
-    // 디자인
-    margin: "0 auto", // 가로 중앙 정렬
-    paddingTop: "50px", // 상단 여백: 50px
-    width:
-      isXLarge ? "1000px" :
-      isLarge ? "740px" :
-      isMedium ? "470px" : "375px",
-  };
-
   // ----------- flex 컨테이너 스타일 -----------
   const flexContainerStyle = {
     // 컨텐츠 정렬
     display: "flex",
     alignItems: "center",
   };
+
+  // ----------- body 스타일 -----------
+  const bodyStyle = {
+    // 상속
+    ...flexContainerStyle,
+
+    // 디자인
+    margin: "200px auto",
+    padding: "0 20px",
+    maxWidth: "1160px",
+    minWidth: "240px",
+    width: "100%",
+
+    // 컨텐츠 정렬
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  // ----------- 보조 문구 스타일 -----------
+  const textStyle = {
+    // 디자인
+    marginBottom: "20px",
+    padding: "8px 20px 4px",
+    width: isXLarge || isLarge ? "500px" : "350px",
+    backgroundColor: "#FFE69C",
+
+    // 글자
+    fontSize: isXLarge || isLarge ? "32px" : "24px",
+    fontWeight: "bold",
+  }
   
   // ----------- 문구 컨테이너 스타일 -----------
   const wordContainerStyle = {
     // 상속
     ...flexContainerStyle,
 
+    // 디자인
+    width: isXLarge ? "100%" : "90%",
+
     // 컨텐츠 정렬
     justifyContent: "center",
-  }
-
-  // ----------- 따옴표 스타일 -----------
-  const quotesStyle = {
-    // 디자인
-    margin: "0 40px",
-    display: isXLarge ? "block" : "none", 
-
-    // 글자
-    fontSize: "160px",
-    color: "#FFFFFF"
   }
 
   // ----------- 메인 문구 스타일 -----------
   const mainWordStyle = {
     // 글자
-    fontSize: 
-      isXLarge ? "42px" :
-      isLarge ? "38px" :
-      isMedium ? "34px" : "30px",
-    textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
+    fontSize: isXLarge || isLarge ? "22px" : "14px",
+  }
+
+  // ----------- 버튼 스타일 -----------
+  const buttonStyle = {
+    // 디자인
+    marginTop: "60px",
+
+    // 글자
+    fontSize: "18px",
+    fontWeight: "bold",
   }
 
   // --------------------------------- css 끝 ---------------------------------
@@ -140,15 +159,15 @@ const MainWord = () => {
   return (
     <>
       <div style={bodyStyle}>
+        <p style={textStyle}># 다른 사람들의 구매 스타일</p>
         <div style={wordContainerStyle}>
-          <div style={quotesStyle}>“</div>
-          <div className="text-center whitespace-pre-wrap font-extrabold tracking-wider">
+          <div className="tracking-wider">
             {phrase.split(' ').map((word, index) => {
               const tagColor = tagColorData.find(tag => tag.name === word)?.color;
               return (
                 <span key={index} style={{
                   ...mainWordStyle,
-                  color: tagColor || "#FFFFFF",
+                  color: tagColor || "#4A4A4A",
                 }}
                 >
                   {word + ' '}
@@ -156,8 +175,12 @@ const MainWord = () => {
               );
             })}
           </div>
-          <div style={quotesStyle}>”</div>
         </div>
+        <Link to="/StatisticPage">
+          <button style={buttonStyle} className="text-amber-500 hover:text-amber-600">
+            더 자세히 알아보기 →
+          </button>
+        </Link>
       </div>
     </>
   );
