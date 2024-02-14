2014d294f3ae77b645e1a7f3e2935a50b705f8db
HocheolHwang <inpofes7849@naver.com>
2024-02-15 07:12:53 +0900

    Refactor:: 코드 작성 방식 개선

--- front-gollajyu/src/components/VotePage/VoteCardItem.jsx
+++ front-gollajyu/src/components/VotePage/VoteCardItem.jsx
@@ -130,9 +130,9 @@ const VoteCardItem = (props) => {
         </Container>
         {/* 버튼을 누르면 생기는 상세페이지 */}
         <div className="h-1/3 w-full flex flex-col justify-center items-center">
-          <p>
-          {selectedItem !== 0 ? `${(count / totalCount * 100).toFixed(2)}%` : '%'}
-          </p>
+          {selectedItem !== 0 && (
+            <p>{`${(count / totalCount * 100).toFixed(2)}%`}</p>
+          )}
           <h2 className="text-lg font-bold mb-2">
             {item.price ? `${item.price.toLocaleString()}원` : ""}
           </h2>
