5426c248af164db085b892e988a4003af896dc85
이시은 <lsy53430@naver.com>
2024-02-15 11:25:48 +0900

    Update:: 상세페이지 모달 - 댓글창 토글 로직 수정

--- front-gollajyu/src/components/VoteDetailPage/VoteDetail.jsx
+++ front-gollajyu/src/components/VoteDetailPage/VoteDetail.jsx
@@ -227,12 +227,12 @@ const VoteDetail = () => {
                 />
               </>
             )}
-            {selectedVoteItem ? (
+            {!selectedVoteItem && (
               <div className="mt-20 text-center">
-                <span className="fontsize-sm">투표에 참여하면 댓글을 확인할 수 있어요!</span>
+                <span className="fontsize-sm">
+                  투표에 참여하면 댓글을 확인할 수 있어요!
+                </span>
               </div>
-            ) : (
-              <></>
             )}
           </div>
         )}
